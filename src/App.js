223fa2cbe2e4037a0e014a2faf593846ca12258c
Oliver <oliver.dr98@gmail.com>
2020-09-06 16:23:22 +0200

    Added custom font for the header and changed the header background

--- src/App.js
+++ src/App.js
@@ -6,23 +6,8 @@ import Login from "./components/login";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Login />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          BOTTOM TEXT
-        </a>
+      <Login />
 
-
-      </header>
     </div>
   );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./login.css";
 
 export default class Login extends Component{
     state = {
@@ -8,9 +9,37 @@ export default class Login extends Component{
 
     render(){
         return (
-            <React.Fragment>
+            /*<React.Fragment>
                 <p> XDDDDDD {'7'  + '7' + '7'}</p>
-            </React.Fragment>
+            </React.Fragment>*/
+        <div>
+
+                <div  className="jumbotron jumbotron-fluid
+                                 text-center
+                                 border-bottom border-dark rounded">
+
+                    <h1>Cafeteria New Wave</h1>
+                </div>
+
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-4">
+                        <h3>Column 1</h3>
+                        <p>Lorem ipsum dolor..</p>
+                    </div>
+                    <div className="col-sm-4">
+                        <h3>Column 2</h3>
+                        <p>Lorem ipsum dolor..</p>
+                    </div>
+                    <div className="col-sm-4">
+                        <h3>Column 3</h3>
+                        <p>Lorem ipsum dolor..</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
         );
     }
 }
