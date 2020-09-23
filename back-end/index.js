e1776a3ab1b6db9f6115a3abbc3a0f42afb58244
Oliver <oliver.dr98@gmail.com>
2020-09-23 03:55:52 +0200

    CI/CD Changes

--- back-end/index.js
+++ back-end/index.js
@@ -1,17 +1,25 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3001;
+// using the http module
+let http = require('http'),
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// look for PORT environment variable,
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// create a simple server
+let server = http.createServer(function (req, res) {
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+
+    });
+
+// listen on the port
+server.listen(port, function () {
+
+    console.log('app up on port: ' + port);
+
 });
