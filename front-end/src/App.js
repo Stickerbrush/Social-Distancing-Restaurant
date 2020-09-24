5827bb38bc35e0b1c5c68009ca76513df73e9a0f
Oliver <oliver.dr98@gmail.com>
2020-09-24 09:41:29 +0200

    Removed unused component imports

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import LoginHeader from "./components/LoginHeader";
-import LoginInputBox from "./components/LoginInputBox";
 import Login from "./components/Login";
 import BarNav from "./components/BarNav";
 
