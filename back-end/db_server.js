9ad7f30645688956bc8824f8e85a455804c11c5d
Oliver <oliver.dr98@gmail.com>
2020-11-04 15:09:04 +0100

    test

--- back-end/db_server.js
+++ back-end/db_server.js
@@ -4,8 +4,18 @@ const port = process.env.PORT || 5000
 
 const modeloCliente = require('./modeloCliente')
 const modeloEmpleado = require('./modeloEmpleado')
+this.app = express()
+this.port = process.env.PORT || 5000
 
 
+this.app.use(express.json())
+this.app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://social-distancing-restaurant.herokuapp.com');
+  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
 
 app.get('/clientes/:cedula/:contrasena', (req, res) => {
   modeloCliente.getCliente(req.params.cedula, req.params.contrasena)
