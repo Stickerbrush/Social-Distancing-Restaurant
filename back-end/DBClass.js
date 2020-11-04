91fafcde9918a7435930eb4aa6035eb1ea035e41
Oliver <oliver.dr98@gmail.com>
2020-11-04 14:28:09 +0100

    make it better

--- back-end/DBClass.js
+++ back-end/DBClass.js
@@ -1,12 +1,24 @@
+const express = require('express')
+const modeloCliente = require('./modeloCliente')
+const modeloEmpleado = require('./modeloEmpleado')
+
 class DBClass {
 
     constructor(props) {
-     this.properties = props
-     this._conn = null
+     this.app = express()
+     this.port = process.env.PORT || 5000
+
     }
 
     connect() {
-     this._conn = driver.connect(this.props)
+      this.app.use(express.json())
+      this.app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', 'https://social-distancing-restaurant.herokuapp.com');
+        //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+        next();
+      });
     }
 
     get conn() {
--- back-end/db_server.js
+++ back-end/db_server.js
@@ -1,18 +1,11 @@
 const express = require('express')
 const app = express()
-const port = (process.env.PORT || 5000)
+const port = process.env.PORT || 5000
 
 const modeloCliente = require('./modeloCliente')
 const modeloEmpleado = require('./modeloEmpleado')
 
-app.use(express.json())
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://social-distancing-restaurant.herokuapp.com');
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-  next();
-});
+
 
 app.get('/clientes/:cedula/:contrasena', (req, res) => {
   modeloCliente.getCliente(req.params.cedula, req.params.contrasena)
