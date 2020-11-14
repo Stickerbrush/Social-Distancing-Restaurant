b0ba9e5365628680e5903fb936bd6a0105599930
Kevin Dominguez <kevindm181998@gmail.com>
2020-11-14 18:06:45 -0500

    Responsive GestionReservasCliente

--- front-end/src/components/GestionReservasCliente.js
+++ front-end/src/components/GestionReservasCliente.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Table, Button, Form } from 'react-bootstrap';
+import { Table, Button, Scrooll, Form } from 'react-bootstrap';
 import "./GestionReservasCliente.css";
 import logo from "../img/Logo-MataHambre.svg";
+import { Scrollbars } from 'react-custom-scrollbars';
 
 export class GestionReservasCliente extends Component {
 
@@ -16,27 +17,29 @@ export class GestionReservasCliente extends Component {
                                 Ingreso al Restaurante
                             </Form.Text>
                         </Form.Group>
-                        <div className = "search-reservation">
-                            <div className = "buttons-imputs">
-                                <Form.Group controlId="formBasicPassword">
+                        <div className="search-reservation">
+                            <div className="buttons-imputs">
+                                
                                     <Form.Control placeholder="ID Reserva" />
                                     <Form.Control placeholder="Telefono (opcional)" />
                                     <Form.Control placeholder="Nombre (opcional)" />
-                                </Form.Group>
-                                <Button
-                                    onClick={this.props.clickCerrarLogin}
-                                    variant="default"
-                                    type="submit"
-                                    style={{ color: "white", background: "#FF834E" }}>
-                                    Buscar
-                                </Button>
-                                <Button
-                                    onClick={this.props.clickCerrarLogin}
-                                    variant="default"
-                                    type="submit"
-                                    style={{ color: "white", background: "#FF834E" }}>
-                                    Ingresar
-                                </Button>
+                                
+                                <div class="buttons-group">
+                                    <Button
+                                        onClick={this.props.clickCerrarLogin}
+                                        variant="default"
+                                        type="submit"
+                                        style={{ color: "white", background: "#FF834E" }}>
+                                        Buscar
+                                    </Button>
+                                        <Button
+                                            onClick={this.props.clickCerrarLogin}
+                                            variant="default"
+                                            type="submit"
+                                            style={{ color: "white", background: "#FF834E"}}>
+                                            Ingresar
+                                    </Button>
+                                </div>
                             </div>
                             <div className="logo-container-reservation">
                                 <img
@@ -49,66 +52,68 @@ export class GestionReservasCliente extends Component {
                         </div>
                     </Form>
                     <br></br>
+                    <div class="table-container">
                     <Table striped bordered hover size="sm" variant="light">
                         <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>CLIENTE</th>
-                            <th>FECHA</th>
-                            <th>TELEFONO</th>
-                            <th># DE MESA</th>
-                            <th>COMIENZA</th>
-                            <th>TERMINA</th>
-                        </tr>
+                            <tr>
+                                <th>ID</th>
+                                <th>CLIENTE</th>
+                                <th>FECHA</th>
+                                <th>TELEFONO</th>
+                                <th># DE MESA</th>
+                                <th>COMIENZA</th>
+                                <th>TERMINA</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>4/11/2020</td>
-                            <td>316 655 2455</td>
-                            <td>1</td>
-                            <td>12:30</td>
-                            <td>17:30</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>4/11/2020</td>
-                            <td>311 444 0022</td>
-                            <td>3</td>
-                            <td>12:30</td>
-                            <td>17:30</td>
-                        </tr>
-                        <tr>
-                            <td> 3</td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                        </tr>
-                        <tr>
-                            <td> 4</td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                        </tr>
-                        <tr>
-                            <td> 5</td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                        </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>4/11/2020</td>
+                                <td>316 655 2455</td>
+                                <td>1</td>
+                                <td>12:30</td>
+                                <td>17:30</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>4/11/2020</td>
+                                <td>311 444 0022</td>
+                                <td>3</td>
+                                <td>12:30</td>
+                                <td>17:30</td>
+                            </tr>
+                            <tr>
+                                <td> 3</td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                            </tr>
+                            <tr>
+                                <td> 4</td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                            </tr>
+                            <tr>
+                                <td> 5</td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                            </tr>
                         </tbody>
                     </Table>
+                    </div>
                 </div>
             </div>
         );
